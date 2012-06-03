75f7836e446c6c8244f2e22164ef59ece4d653fd
Mark <hurleydies@gmail.com>
2012-06-03 20:44:08 +0100

    \!tweet

---
+++ twitter.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/ruby
+require "xmlrpc/server"
+require 'rss/1.0'
+require 'rss/2.0'
+require 'open-uri'
+
+serv = XMLRPC::CGIServer.new
+
+serv.add_handler("query") do |malcolms_url, some_crazy_random_string, message|
+  source = "http://twitter.com/statuses/user_timeline/" + message + ".rss?count=1"
+  content = ""
+  result = ""
+  if message.match('\s') then
+    result= "I'm sorry, but I can't let you do that, Dave"
+  else
+    begin
+      open(source) { |s| content = s.read }
+      rss = RSS::Parser.parse(content, false)
+      result = rss.items[0].description
+      result.sub!(/[^:]*: /, '')
+    rescue OpenURI::HTTPError
+      result = message + " is not cool enough for twitter"
+    end
+  end
+  result
+end
+  
+serv.serve
