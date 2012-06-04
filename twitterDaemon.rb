d954221c91bac26e5cff400d21ab314a14186d43
Mark <hurleydies@gmail.com>
2012-06-04 02:06:36 +0100

    tidied up and removed some common configuration details

--- twitterDaemon.rb
+++ twitterDaemon.rb
@@ -7,32 +7,30 @@ require 'base64'
 require 'tweetstream'
 
 malcolm = 'malcy'
-password = 'TOPSEKRIT'
-xmlrpc_url = "http://some.stupid.domain"
-call_url = "http://some.stupid.domain"
+xmlrpc_url = 'REDACTED'
+call_url = 'REDACTED'
+consumer_key = 'REDACTED'  
+consumer_secret = 'REDACTED'
+oauth_token = 'REDACTED'
+oauth_token_secret = 'REDACTED'
 
-puts "configuring twitter"
 Twitter.configure do |config|
-  config.consumer_key = ''  
-  config.consumer_secret = ''
-  config.oauth_token = ''
-  config.oauth_token_secret = ''
+  config.consumer_key = consumer_key
+  config.consumer_secret = consumer_secret
+  config.oauth_token = oauth_token
+  config.oauth_token_secret = oauth_token_secret
 end
-puts "configuring tweetstream"
 TweetStream.configure do |config|
-  config.consumer_key = ''  
-  config.consumer_secret = ''
-  config.oauth_token = ''
-  config.oauth_token_secret = ''
+  config.consumer_key = consumer_key
+  config.consumer_secret = consumer_secret
+  config.oauth_token = oauth_token
+  config.oauth_token_secret = oauth_token_secret
   config.auth_method = :oauth
 end
 
-puts "Starting daemon"
 daemon = TweetStream::Daemon.new
 daemon.userstream do |status|
-  puts "got status"
   if status.text.match("^@#{malcolm}") then
-    puts "matched reply"
     status.text.match("@#{malcolm} \(.*\)")
     message = $1
     user = status.user.screen_name
@@ -49,12 +47,8 @@ daemon.userstream do |status|
     rescue Twitter::Error::Forbidden
       # This probably meant we tried to post the same status twice. Just ignore
       # it.
-      sleep(nap_length)
     rescue Timeout::Error
       puts "Timeout Error, ignoring\n"
-      sleep(nap_length)
     end
-  else
-    puts "No match!\n"
   end
 end
